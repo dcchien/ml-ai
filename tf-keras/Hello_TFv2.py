1ba0e491e11d6da7d12f0cbff16e7058803923b8
Dave C <12160407+dcchien@users.noreply.github.com>
2019-08-04 19:21:23 -0400

    add TFv2 example

--- tf-keras/Hello_TFv2.py
+++ tf-keras/Hello_TFv2.py
@@ -1,12 +1,23 @@
 # simple example of TF with TFv2 Beta1
 # 
+from __future__ import absolute_import, division, print_function, unicode_literals
+
 import tensorflow as tf
 from tensorflow import keras
 
 print("TF version:", tf.__version__)
 print("Keras version:",keras.__version__)
 
+# @tf.function decorator - function a like an op
+@tf.function
+def add(a, b):
+  return a + b
+
+print(add(tf.constant(20), tf.constant(80)))
+print(add(10,60))
+
 #need to add this line for TFv2 Beta
+#disable eagle execution
 tf.compat.v1.disable_eager_execution()
 
 hello = tf.constant('Hello, Tensorflow')
@@ -18,4 +29,4 @@ print(sess.run(hello))
 
 a = tf.constant(10)
 b = tf.constant(70)
-print(sess.run(a + b))
+print("tfv1 session run", sess.run(a + b))
